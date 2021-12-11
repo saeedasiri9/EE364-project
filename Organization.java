9a6aed062a831eee60c08d1a3446c1acd8b90b10
OmarAB7 <95591945+OmarAB7@users.noreply.github.com>
2021-12-11 17:59:01 +0300

    Add files via upload
    full

--- Organization.java
+++ Organization.java
@@ -162,7 +162,141 @@ public class Organization {
 
 				checkIfDone(); // calling CheckIfDone method to check if the a certain governmental treatment is done (go to the end of the main class to see the method)
 
+				for (int i = 0; i < CompletedGovernmentalTreatments.size(); i++) { /* loop to store all the finished governmental treatment in Archive
+				                                                                                                 	and to remove it from CompletedGovernmentalTreatments Arraylist */
+					archive.getArchiveOfGovernmentalTreatment().add(CompletedGovernmentalTreatments.get(i));
+					CompletedGovernmentalTreatments.remove(i);
+				}
+
+				for (int i = 0; i < archive.getArchiveOfGovernmentalTreatment().size(); i++) { /* the Actual time for the governmental treatment 
+					                                                                                                      will be increased based on the error percentage */
+					
+					if (archive.getArchiveOfGovernmentalTreatment().get(i).getErrorPercent() != 0) { // the archive will work on their governmental treatment that have errors
+
+						archive.ArchMethod(archive.getArchiveOfGovernmentalTreatment().get(i));
+
+						double Time = archive.getArchiveOfGovernmentalTreatment().get(i).getActualTime() + 1; // the actual time for the governmental treatment will be increased
+
+						archive.getArchiveOfGovernmentalTreatment().get(i).setActualTime(Time); // set the actual time after the increase
+
+					}
+
+					else if (archive.getArchiveOfGovernmentalTreatment().get(i).getErrorPercent() == 0) { /* if there are no errors in the governmental treatment the program will apply the PrintArch method
+					                                                                                      to the governmental treatment and will remove it from the ArchiveOfGovernmentalTreatment array list 
+					                                                                                      and add it to the CompleteArchiveOfGovernmentalTreatment array list in the archive department class*/
+						governmentalTreatment Gov = archive.getArchiveOfGovernmentalTreatment().get(i);
+						archive.PrintArch(Gov); //see the method in archive class to know more about it
+						archive.getCompleteArchiveOfGovernmentalTreatment().add(Gov);
+						archive.getArchiveOfGovernmentalTreatment().remove(i);
+					}
+
+				}
+
+				TableViewer table = new TableViewer(archive.getIndexArchiveOfGovernmentalTreatment(), // Creating object from the TableViewer class 
+						archive.getIDArchiveOfGovernmentalTreatment());                                         // and give it the index of governmental treatment and ID in the archive
+
+				if ((archive.getCompleteArchiveOfGovernmentalTreatment().size() != 0) && (HeWantYearReport == 0)) {
+					int ans2 = 2;
+
+					do { // after finishing the first governmental treatment, the user will be asked if he wants to check it from the archive
+
+						System.out.println(
+								"Do you want to check the archive for a Governmental Treatment you want?: Enter Y if yes , N if no "
+										+ "\n");
+
+						String ans3 = scan.next();
+						;
+
+						if (ans3.equals("y") || ans3.equals("Y")) {
+
+							table.viewTable(10, 10); // To show the user all the governmental treatment in archive by ID
+
+							System.out.println("Pick your Governmental Treatment by ID: " + "\n");
+							try {
+								ans2 = scan.nextInt();
+							} catch (Exception e) {
+								System.out.println("Sorry, wrong input the program will shutdown" + "\n");
+								System.exit(0);
+							}
+							for (int i = 0; i < archive.getCompleteArchiveOfGovernmentalTreatment().size(); i++) {
+
+								governmentalTreatment ArchGov = archive.getCompleteArchiveOfGovernmentalTreatment().get(i);
+
+								if (ArchGov.getID() == ans2) {
+
+									System.out.println(ArchGov);
+
+								} else
+									continue;
+
+							}
+
+						} else if (ans3.equals("n") || ans3.equals("N")) // if the user don't want to continue the simulation
+							break;
+						else {
+							System.out.println(
+									"Sorry you entered wrong input, The program will skip this Quesion" + "\n");
+							break;
+						}
+
+					} while (true);
+
+				} else
+					System.out.println("There are no Complete Govermental Treatment yet " + "\n");
 				
+				System.out.println("-----------------------------------------------------------------------" + "\n");
+				
+			} else if (ans1 == 'n' || ans1 == 'N') {
+				System.out.println("End of Simulation" + "\n");
+				System.exit(0);
+
+			}
+			if (HeWantYearReport == 0) { // if the user wants to see the yearly report
+				System.out.println("Do you want to skip to the year report: Enter (1) if yes , (0) if no " + "\n");
+				try {
+					HeWantYearReport = scan.nextInt();
+				} catch (Exception e2) {
+					System.out.println("Sorry you entered wrong input, The program will skip this Quesion " + "\n");
+
+				}
+				if (HeWantYearReport != 1 && HeWantYearReport != 0) {
+					while (true) {
+						if (HeWantYearReport != 1 && HeWantYearReport != 0) {
+							System.out.println("plese enter ether 1 or 0" + "\n");
+
+							try {
+								HeWantYearReport = scan.nextInt();
+							} catch (Exception e2) {
+								System.out.println(
+										"Sorry you entered wrong input, the program will skip to year report: " + "\n");
+								break;
+
+							}
+						} else
+							break;
+					}
+				}
+
+			}
+
+		}
+
+		double R_Tot = 0;
+		double A_Tot = 0;
+		for (int i = 0; i < archive.getCompleteArchiveOfGovernmentalTreatment().size(); i++) {  //calculating the average increase in time required for each treatment
+
+			R_Tot = R_Tot + archive.getCompleteArchiveOfGovernmentalTreatment().get(i).getRequiredTime();
+			A_Tot = A_Tot + archive.getCompleteArchiveOfGovernmentalTreatment().get(i).getActualTime();
+
+		}
+
+		double Increase_In_Time = ((A_Tot / R_Tot) * 100) - 100;
+
+		System.out.println("We reach 360 days and this is the year report: " + "\n"); //printing the year report
+		System.out.println("Total number of Governmental Treatments: "
+				+ archive.getCompleteArchiveOfGovernmentalTreatment().size() + " Governmental Treatment" + "\n");
+		System.out.println("The average increase in time required: " + Increase_In_Time + " %" + "\n");
+
 	}
 
 	private static void checkIfDone() {
